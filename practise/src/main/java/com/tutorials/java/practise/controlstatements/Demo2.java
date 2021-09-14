0c120ba743f5e8e16e73b4f81f5765b169731097
bhagat <bhagat@srritsolutions.com>
2021-09-14 15:00:27 +0530

    added changes

---
+++ practise/src/main/java/com/tutorials/java/practise/controlstatements/Demo2.java
@@ -0,0 +1,20 @@
+package com.tutorials.java.practise.controlstatements;
+
+public class Demo2 {
+    /*
+    * Basic looping programs
+    *
+    * */
+    public static void main(String[] args) {
+        /* 1.for loop
+        *  the for loop is a control flow statement that executes a block of code several times.
+        *  It iterates the same code repeatedly until the specified condition is true.
+        * for(initialization;condition;increment or decrement){
+        *
+        *   // statements
+        *
+        * }
+        * */
+
+    }
+}
